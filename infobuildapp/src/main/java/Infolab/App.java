caaa79e05832ca7e7cb503254741411bf74974ba
akhilsighakolli <sighakolliakhilguptha@gmail.com>
2023-11-19 12:42:32 +0530

    file name called infobuildapp added

---
+++ infobuildapp/src/main/java/Infolab/App.java
@@ -0,0 +1,13 @@
+package Infolab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
